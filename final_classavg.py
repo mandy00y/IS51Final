6bee5dd5d0fe545c56b4dd1ce736c12b37d423b9
Your Name <you@example.com>
2022-05-19 16:49:07 -0700

    adding pseudo-code

--- final_classavg.py
+++ final_classavg.py
@@ -6,18 +6,48 @@ and the percentage of grades that are above the average grade.
 
 
 In order to find the number of grades, 
-create a loop through the list of grades
 output the total count of grades in the list.
 
 In order to find the average grade, 
+create a loop through the list of grades
 add all the grades / total count of grades
 then output the average grade.
 
-Then, initiaite an iterator (counter), and an accumulator
+Then, initiate an iterator (counter), and an accumulator
 In order to find the percentage of grades above the average grade,
+create loop through the list of grades
 add all grades > average grade, 
 increment the counter by 1, 
 then divide it by total count of grades, and
 multiply 100 to get the percentage.
 
+"""
+
+
+"""
+def main(): 
+
+open file "Final.txt"
+grades = [line.rstrip()]
+close file
+
+create loop
+    add all grades / total count of grades
+    output average
+
+def calculate_percent_above_average():
+
+initiate an iterator
+create a loop
+    grade > average grade:
+        add / total count of grades
+        increment counter by 1
+        multiply by 100
+
+print "Number if grades: "
+print "Average grade: "
+print "Percentage of grades above average: "
+
+
+main()
 """
