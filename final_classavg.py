1472087ff51c8d2939234b26678495b508eabd7b
Your Name <you@example.com>
2022-05-19 16:31:25 -0700

    adding structured-english

--- final_classavg.py
+++ final_classavg.py
@@ -1,1 +1,23 @@
 
+"""
+First, we will have to open the file 'Final.txt' that contains the students' grades.
+We will then write a program that displays the number of grades, the average grade, 
+and the percentage of grades that are above the average grade.
+
+
+In order to find the number of grades, 
+create a loop through the list of grades
+output the total count of grades in the list.
+
+In order to find the average grade, 
+add all the grades / total count of grades
+then output the average grade.
+
+Then, initiaite an iterator (counter), and an accumulator
+In order to find the percentage of grades above the average grade,
+add all grades > average grade, 
+increment the counter by 1, 
+then divide it by total count of grades, and
+multiply 100 to get the percentage.
+
+"""
